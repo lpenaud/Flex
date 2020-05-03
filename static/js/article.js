74a22eeb6c38f7dc761c8e67505f70cd5de0092b
lpenaud <l.penaud@zaclys.net>
2020-05-03 16:09:09 +0200

    Final toc style ?

--- static/js/article.js
+++ static/js/article.js
@@ -1,10 +1,16 @@
-(function () {
-  var topicTitle = document.querySelector('div.topic > p.topic-title');
-  var topicCheckbox = document.createElement('input');
-  topicCheckbox.type = 'checkbox';
-  topicCheckbox.classList.add('toc-toogle');
-  topicTitle.parentElement.insertBefore(topicCheckbox, topicTitle);
-  topicTitle.addEventListener('click', function () {
-    topicCheckbox.checked = !topicCheckbox.checked;  
+function topic(title) {
+  console.log(title.textContent);
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  title.parentElement.insertBefore(checkbox, title);
+  title.addEventListener('click', function () {
+    checkbox.checked = !checkbox.checked;
   });
+}
+
+(function () {
+  var titles = document.querySelectorAll('div.topic > p.topic-title');
+  for (var i = 0; i < titles.length; i++) {
+    topic(titles[i]);
+  }
 })();
